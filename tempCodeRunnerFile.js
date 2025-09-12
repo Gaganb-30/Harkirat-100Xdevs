2247564d3fb105bb166ce6c8ef994ca67deecc0c
Gagan Bisht <gaganbisht286@gmail.com>
2025-09-12 23:44:08 +0530

    added medium-blog

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-function displayClock() {
-  const curr = new Date();
-  const time = `${curr.getHours()} : ${curr.getMinutes()} : ${curr.getSeconds()}`;
-  console.clear();
-  console.log(time);
-}
-
-setInterval(displayClock, 1000);
